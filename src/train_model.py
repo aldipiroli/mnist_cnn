eec36e7a9c1f7d3c8df3800607d2282c9a5c32a5
aldipiroli <aldipiroli@gmail.com>
2020-12-13 17:21:34 +0100

    [ADD] Stated adding new modle

--- src/train_model.py
+++ src/train_model.py
@@ -39,6 +39,10 @@ def LoadData(bs, device):
     )
 
     n, c = x_train.shape
+    x_train = x_train.view((x_train.shape[0], 1,  28, 28))
+
+    print("Ecco", y_train.shape)
+    # print("Ecccooolo ", x_train.shape)
 
     train_ds = TensorDataset(x_train, y_train)
     train_dl = DataLoader(train_ds, batch_size=bs)
@@ -49,33 +53,44 @@ def LoadData(bs, device):
     return train_dl, valid_dl, n, c
 
 
+
 class Mnist_CNN(nn.Module):
     def __init__(self):
         super(Mnist_CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        xb = x.view(-1, 1, 28, 28)
-        xb = F.relu(self.conv1(xb))
-        xb = F.relu(self.conv2(xb))
-        xb = F.relu(self.conv3(xb))
-        xb = F.avg_pool2d(xb, 4)
-        return xb.view(-1, xb.size(1))
-
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 if __name__ == "__main__":
     use_cuda = False
     device = torch.device("cuda" if use_cuda else "cpu")
     
     bs = 64
-    epochs = 10
+    epochs = 1
     lr = 0.1
 
     train_dl, valid_dl, n, c = LoadData(bs, device)
     
-    loss_func = F.cross_entropy
+    loss_func = F.nll_loss
+
 
     model = Mnist_CNN().to(device)
     opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
@@ -84,17 +99,23 @@ if __name__ == "__main__":
     for epoch in range(epochs):
         model.train()
         for count, (xb, yb) in enumerate(train_dl):
+            print("Da shape: ", xb.shape)
             if count % 100 == 0:
                 print("Epoch: ", epoch, " #: ", count)
+            
 
-            x_show = xb[0, :]
-            print(x_show.shape)
-            plt.imshow(x_show.view((28,28)),  cmap="gray")
-            plt.show()
-            input()
-            xb.to(device)
-            yb.to(device)
 
+            # x_show = xb[0, :]
+            # print(x_show.shape)
+            # plt.imshow(x_show.view((28,28)),  cmap="gray")
+            # plt.show()
+            # input()
+            # xb.to(device)
+            # yb.to(device)
+            # print(xb.shape)
+            xb = xb.view((bs, 1, 28, 28))
+            # print(xb.shape)
+            # input()
 
             pred = model(xb)
             loss = loss_func(pred, yb)
@@ -105,7 +126,12 @@ if __name__ == "__main__":
 
         model.eval()
         with torch.no_grad():
-            valid_loss = sum(loss_func(model(xb), yb) for xb, yb in valid_dl)
+            print("The loss is:")
+            val_loss = 0
+            for xb, yb in valid_dl:
+                xb = xb.view((2*bs, 1, 28, 28))
+                val_loss += loss_func(model(xb), yb)
+            # valid_loss = sum(loss_func(model(xb), yb) for xb, yb in valid_dl)
 
         print(epoch, valid_loss / len(valid_dl))
 
