1713748b6c39f5b3a8650940417185b5896d7b9e
aldipiroli <aldipiroli@gmail.com>
2020-12-13 14:58:56 +0100

    [ADD] Added centering of the image

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -53,16 +53,6 @@ def LoadData(bs):
     return train_dl, valid_dl, n, c
 
 
-def LoadImage(file):
-    img = cv2.imread(file, 0)
-    img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
-    img = np.floor(img / 255)
-    img = torch.from_numpy(img)
-    img = img.view(1, 784)
-    img = img.float()
-    return img
-
-
 class ImageHandler:
     def __init__(self):
         # super().__init__()
@@ -78,11 +68,9 @@ class ImageHandler:
             cv2.imshow('test draw', self.img)
             if cv2.waitKey(1) & 0xFF == 27:
                 break
-            # self.EvaluateImage()
-
         cv2.destroyAllWindows()
-        
-    def line_drawing(self,event, x, y, flags, param):
+
+    def line_drawing(self, event, x, y, flags, param):
 
         if event == cv2.EVENT_LBUTTONDOWN:
             self.drawing = True
@@ -99,15 +87,51 @@ class ImageHandler:
             cv2.line(self.img, (self.pt1_x, self.pt1_y),
                      (x, y), color=(255, 255, 255), thickness=2)
 
-    
     def ProcessImage(self):
+        # self.Normalize()
+        self.FindBoundingBox()
+
+        return self.img
+
+    def FindBoundingBox(self):
+        # a,b,c,d = 0
+        x_min = 1000
+        x_max = -1
+        y_min = -1
+        y_max = 1000
+
+        print(self.img.size)
+        for x in range(27):
+            for y in range(27):
+                if(self.img[x, y] > 0):
+                    if(x > x_max):
+                        x_max = x
+
+                    if(x < x_min):
+                        x_min = x
+
+                    if(y > y_max):
+                        y_max = x
+
+                    if(y < y_min):
+                        y_min = y
+
+    def Normalize(self):
         self.img = cv2.resize(self.img, (28, 28), interpolation=cv2.INTER_AREA)
         self.img = np.floor(self.img / 255)
         self.img = torch.from_numpy(self.img)
         self.img = self.img.view(1, 784)
         self.img = self.img.float()
-        return self.img
 
+        # mask = np.zeros(image.shape, dtype=np.uint8)
+        # ROI = image[y:y+h, x:x+w]
+        # x = width//2 - ROI.shape[0]//2
+        # y = height//2 - ROI.shape[1]//2
+        # mask[y:y+h, x:x+w] = ROI
+
+        # cv2.imshow('ROI', ROI)
+        # cv2.imshow('mask', mask)
+        # cv2.waitKey()
 
     def EvaluateImage(self):
         img = self.ProcessImage()
@@ -117,6 +141,28 @@ class ImageHandler:
         print("Prediction: ", indx)
 
 
+def LoadImage(file):
+    img = cv2.imread(file, 0)
+    img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+    img = np.floor(img / 255)
+    img = torch.from_numpy(img)
+    img = img.view(1, 784)
+    img = img.float()
+    return img
+
+
+def CenterImage(file):
+    img = cv2.imread(file, 0)
+    height, width = img.shape
+    x, y, w, h = cv2.boundingRect(img)
+
+    buff = 20
+    img = img[y-buff:y+h+buff, x-buff:x+w+buff]
+
+    plt.imshow(img)
+    plt.show()
+
+
 
 if __name__ == "__main__":
     model = Mnist_CNN()
@@ -124,19 +170,20 @@ if __name__ == "__main__":
         "/home/aldi/workspace/mnist_cnn/src/model/model_cnn.pt"))
     model.eval()
 
-    # file1 = "/home/aldi/workspace/mnist_cnn/src/data/mnist/sample_mnist/tensor([3]).png"
-    # file2 = "/home/aldi/workspace/mnist_cnn/src/data/mnist/sample_nums/num_8.png"
+    file1 = "/home/aldi/workspace/mnist_cnn/src/data/mnist/sample_mnist/tensor([3]).png"
+    file2 = "/home/aldi/workspace/mnist_cnn/src/data/mnist/sample_nums/num_7.png"
     # img = LoadImage(file2)
-    handler = ImageHandler()
-    img = handler.ProcessImage()
+    CenterImage(file2)
 
+    # handler = ImageHandler()
+    # img = handler.ProcessImage()
 
-    pred = model(img)
-    indx = torch.argmax(pred, dim=1)
-    plt.imshow(img.view(28, 28))
-    print("Prediction: ", indx)
-    plt.show()
+    # pred = model(img)
+    # indx = torch.argmax(pred, dim=1)
 
+    # print("Prediction: ", indx)
+    # plt.imshow(img.view(28, 28))
+    # plt.show()
 
     # train_dl, valid_dl, n, c = LoadData(1)
 
