acd56c363af7ebff603a495789dd3eb2212604eb
aldipiroli <aldipiroli@gmail.com>
2020-12-13 11:04:28 +0100

    [ADD] Added more complex model

--- src/train_model.py
+++ src/train_model.py
@@ -51,18 +51,42 @@ def LoadData(bs):
 
 class Mnist_CNN(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
-        self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+        super(Mnist_CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-    def forward(self, xb):
-        xb = xb.view(-1, 1, 28, 28)
-        xb = F.relu(self.conv1(xb))
-        xb = F.relu(self.conv2(xb))
-        xb = F.relu(self.conv3(xb))
-        xb = F.avg_pool2d(xb, 4)
-        return xb.view(-1, xb.size(1))
+    def forward(self, x):
+        x = x.view(-1, 1, 28, 28)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    # def __init__(self):
+    #     super().__init__()
+    #     self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+    #     self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
+    #     self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+
+    # def forward(self, xb):
+    #     xb = xb.view(-1, 1, 28, 28)
+    #     xb = F.relu(self.conv1(xb))
+    #     xb = F.relu(self.conv2(xb))
+    #     xb = F.relu(self.conv3(xb))
+    #     xb = F.avg_pool2d(xb, 4)
+    #     return xb.view(-1, xb.size(1))
 
 
 
